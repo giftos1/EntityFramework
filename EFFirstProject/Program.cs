a1240dac478c97c5bd3ec7d98f43ac078ace7dd2
giftos1 <giftsydney676@gmail.com>
2025-10-18 14:58:51 +0800

    correct db path

--- EFFirstProject/Program.cs
+++ EFFirstProject/Program.cs
@@ -1,12 +1,41 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
 namespace EFFirstProject
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var db = new BloggingContext();
 
+            // Note: This sample requires the database to be created before running.
+            Console.WriteLine($"Database path: {db.DbPath}");
+
+            // Create
+            Console.WriteLine("Inserting a new blog");
+            db.Add(new Blog { Url = "https://giftos.com/blogs/1" });
+            await db.SaveChangesAsync();
+
+            // Read
+            Console.WriteLine("Querying for a blog");
+            var blog = await db.Blogs
+                .OrderBy(blog => blog.BlogId)
+                .FirstAsync();
+
+            // Update
+            Console.WriteLine("Updating the blog and adding a post");
+            blog.Url = "https://giftos.com/blogs/blogs/1";
+            blog.Posts.Add(
+                new Post { Title = "Fish and Chips", Content = "Great recipe for fish and chips." }
+                );
+            await db.SaveChangesAsync();
+
+            // Delete
+            Console.WriteLine("Delete the blog");
+            db.Remove(blog);
+            await db.SaveChangesAsync();
         }
     }
 }
