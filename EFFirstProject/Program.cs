fe87f86c873cbda3dc6d6e4283d46e6f5d98ca19
giftos1 <giftsydney676@gmail.com>
2025-10-18 15:45:25 +0800

    Use CRUD operations on DB

--- EFFirstProject/Program.cs
+++ EFFirstProject/Program.cs
@@ -24,14 +24,36 @@ namespace EFFirstProject
                 .OrderBy(blog => blog.BlogId)
                 .FirstAsync();
 
+            
             // Update
             Console.WriteLine("Updating the blog and adding a post");
-            blog.Url = "https://giftos.com/blogs/blogs/1";
+            blog.Url = "https://giftos.com/blogs/1";
             blog.Posts.Add(
                 new Post { Title = "Fish and Chips", Content = "Great recipe for fish and chips." }
                 );
+            //blog.Posts.Add(
+            //    new Post { Title = "Roast Beef", Content = "How to cook the perfect roast beef." }
+            //    );
             await db.SaveChangesAsync();
+            Console.WriteLine(blog.Url);
+            Console.WriteLine(blog.Posts.Count);
 
+            // Query for all posts
+            var posts = await db.Posts
+                .OrderBy(post => post.PostId)
+                .ToListAsync();
+
+            Console.WriteLine("All posts:");
+            posts.ForEach(post => Console.WriteLine($"{post.PostId}. {post.Title}, {post.Content}"));
+            Console.WriteLine();
+
+            // Query for all blogs
+            var blogs = await db.Blogs
+                .OrderBy(blog => blog.BlogId)
+                .ToListAsync();
+            Console.WriteLine("All blogs:");
+            blogs.ForEach(blog => Console.WriteLine($"{blog.BlogId}. {blog.Url}"));
+            Console.WriteLine();
             // Delete
             Console.WriteLine("Delete the blog");
             db.Remove(blog);
